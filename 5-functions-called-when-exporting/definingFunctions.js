45c5ccf79cc0c87d27235dd477aa2511bd236772
Ishan <ishanwijes@gmail.com>
2022-07-15 17:51:00 +1000

    activity 5 completed - exporting results in functions being executed

---
+++ 5-functions-called-when-exporting/definingFunctions.js
@@ -0,0 +1,25 @@
+
+// define a couple of functions
+const sumTwoValues = function(num1, num2) {
+    console.log(num1 + num2);
+}
+
+const subtractTwoValues = function(num1, num2) {
+    console.log(num1 - num2);
+}
+
+
+
+// call these functions
+sumTwoValues(5, 4);
+subtractTwoValues(5, 4);
+
+
+
+
+
+
+
+
+
+
---
+++ 5-functions-called-when-exporting/exportingOtherFile.js
@@ -0,0 +1,7 @@
+
+// if a function is executed in whatever file is being exported, whether it's saved in a variable or simply just exported...
+// ALL THOSE FUNCTIONS WILL BE EXECUTED AGAIN IN THIS FILE
+const exportedFunctions = require('./definingFunctions');
+
+// require('./definingFunctions');
+
