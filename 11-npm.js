c1657e8aa8366a0de7b7c534493170df420abeb0
Ishan <ishanwijes@gmail.com>
2022-07-18 18:23:57 +1000

    activity 11 half completed - set up npm commands

---
+++ 11-npm.js
@@ -0,0 +1,20 @@
+
+// npm means node package manager
+
+// a dependency, a module, a package, basically refers to the same thing. They can be used interchangeably.
+
+
+
+// terminal commands
+
+// npm --version or npm --v
+// this gives your current version of npm
+
+// npm i packageName
+// we can download a package/dependency/module for JUST ONE PARTICULAR PROJECT
+
+// npm install -g packageName
+// this downloads the package as a global dependency, use it in any project
+
+
+
