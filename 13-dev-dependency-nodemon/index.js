18dca0c262c53a11b1e417b651aa82cb5dbda9c8
Ishan <ishanwijes@gmail.com>
2022-07-20 19:21:37 +1000

    activity 13 completed - dev dependency nodemon

---
+++ 13-dev-dependency-nodemon/index.js
@@ -0,0 +1,13 @@
+
+// nodemon is basically the same as live server, it will continually watch for changes we make in our index.js file, and then automatically change the terminal window
+
+// pretty cool because you have to click on the terminal or off the file for changes to come into effect
+
+console.log('hello world!');
+
+console.log('what up homie');
+
+
+// to stop nodemon, simply use control + C
+
+// and obviously if you want to delete the devDependency, just use npm uninstall packageName
