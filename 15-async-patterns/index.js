4b9c5b376a4b8d215a84cd105d3dd7eb2b702da9
Ishan <ishanwijes@gmail.com>
2022-07-23 22:39:12 +1000

    activity 15 completed - using async/await and promises in node

---
+++ 15-async-patterns/index.js
@@ -0,0 +1,57 @@
+
+// using promises
+
+const fs = require('fs');
+
+
+
+// creating a function that returns a promise, that way we can keep reusing this function and just change the arguments so that it's reusable
+const readText = function(filePath) {
+
+    return new Promise((resolveCallback, rejectCallback) => {
+
+        fs.readFile(filePath, 'utf8', (error, data) => {
+            if(error) {
+                rejectCallback(error);
+            } else {
+                resolveCallback(data);
+            }
+        })
+
+    })
+
+}
+
+// because this function returns a promise, you can chain .then() and .catch()
+// readText('../1-intro.js').then(data => {
+//     console.log(data);
+//     console.log('it worked!');
+// }).catch(error => {
+//     console.log(error);
+//     console.log('it did not work');
+// })
+
+
+
+// using async-await to do multiple asynchronous tasks in order
+// we would have to do a lot of chaining and many .then()'s to do multiple asynchronous tasks one after the other
+// for example, let's say we want to read a file, then read another new file, then write a file
+// this is made easy with async-await 
+const getTextAsyncAwait = async function() {
+    try {
+        // wait until the promise returns resolved, and only then, execute 'console.log(FirstFileRead)'
+        // this is crucial because if we don't wait until this promise is resolved, if we try and log firstFileRead, it will be undefined
+        const firstFileRead = await readText('../1-intro.js');
+        console.log(firstFileRead);
+
+        // now let's read our second file
+        const secondFileRead = await readText('../2-globals.js');
+        console.log(secondFileRead);
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+getTextAsyncAwait('../1-intro.js');
